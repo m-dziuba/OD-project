2f972de80593508a44c3f8b74a9ef89165a7deb0
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2022-03-08 22:16:43 +0100

    Add get_json, remake get_links

--- webscraper/main.py
+++ webscraper/main.py
@@ -7,37 +7,39 @@ from tqdm import tqdm
 # TODO Rethink the way that urls and other data is obtained.
 #  Current system will fail as soon as the website changes
 
+base_url = "https://www.otodom.pl"
+
+
+def get_json(url):
+    source = requests.get(url).content
+    soup = BeautifulSoup(source, "lxml")
+    json_string = soup.find("script", id="__NEXT_DATA__")
+    json_obj = json.loads(json_string.text)
+    return json_obj["props"]["pageProps"]
+
 
 def get_links():
-    base_url = "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa/"
+    relative_url = "/pl/oferty/sprzedaz/mieszkanie/warszawa/"
     districts = ["zoliborz"]
-    pages = 1
-    offer_urls = []
+    json_obj = get_json(base_url + relative_url + districts[0])
+    pages = json_obj["data"]["searchAds"]["pagination"]["totalPages"]
 
+    offer_urls = []
     for i in tqdm(range(1, pages + 1)):
-        url = base_url + districts[0] + "?page=" + str(i)
-        source = requests.get(url).content
-        soup = BeautifulSoup(source, "lxml")
-        listings = soup.find("div", role="main").find_all(attrs={"data-cy": "search.listing"})
-        # promoted = listings[0].find("ul").find_all("li") # TODO Delete or find a use case
-        regular = listings[1].find("ul").find_all("li")
-        for offer in regular:
-            try:
-                link = offer.a["href"]
-                offer_urls.append(link)
-            except Exception:
-                pass
+        url = base_url + relative_url + districts[0] + "?page=" + str(i)
+        page_json = get_json(url)
+        for item in page_json["data"]["searchAds"]["items"]:
+            offer_urls.append(base_url + "/pl/oferta/" + item["slug"])
 
     return offer_urls
 
 
 def get_images(urls):
-    base_url = "https://www.otodom.pl"
     image_urls = []
 
-    for url in tqdm(urls):
+    for relative_url in tqdm(urls):
         current_url_images = []
-        source = requests.get(base_url + url).content
+        source = requests.get(base_url + relative_url).content
         soup = BeautifulSoup(source, "lxml")
 
         json_string = soup.find("script", id="__NEXT_DATA__")
@@ -52,11 +54,11 @@ def get_images(urls):
 
 
 def get_descriptions(urls):
-    base_url = "https://www.otodom.pl"
+
     descriptions = []
 
-    for url in tqdm(urls):
-        source = requests.get(base_url + url).content
+    for relative_url in tqdm(urls):
+        source = requests.get(base_url + relative_url).content
         soup = BeautifulSoup(source, "lxml")
 
         description_paragraphs = soup.find("section", role="region").find(
@@ -67,7 +69,19 @@ def get_descriptions(urls):
     return descriptions
 
 
+def get_details():
+    with open("example.html") as source:
+        soup = BeautifulSoup(source, "lxml")
+
+    tags_of_interest = ["characteristics", "dateCreated", "dateModified",
+                        "description", "featuresByCategory", "location", "statistics"]
+    json_string = soup.find("script", id="__NEXT_DATA__")
+    json_obj = json.loads(json_string.text)
+    json_features = json_obj["props"]["pageProps"]["ad"]["characteristics"]
+    print(print(json.dumps(json_features, indent=4)))
+
 if __name__ == "__main__":
-    links_list = get_links()
+    get_links()
     # get_images(links_list)
     # get_descriptions(links_list)
+    # get_details()
